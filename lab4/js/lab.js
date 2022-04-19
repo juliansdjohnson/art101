7c28410fb5a52421b6326efe78b6b67fb8d147de
Julian Johnson <jusjohns@ucsc.edu>
2022-04-18 19:59:08 -0700

    Added lab4.html

---
+++ lab4/js/lab.js
@@ -0,0 +1,23 @@
+// Tree Planter!.
+// Plants a tree from an acorn, detailing each step.
+// @file   This files defines the MyClass class.
+// @author Julian Johnson, Bryan Long
+// @since  4.18.22
+
+// Collect acorns.
+// Determine viable acorns:
+	// Place acorns in cold water.
+	// If acorns sink, collect them
+// Fill small pots with potting mix.
+// Plant two acorns sideways in each pot, one inch deep.
+// Cover pots with screen.
+// Keep the soil moist:
+  // If it's winter, don't water them
+  // If it's spring, frequently water saplings until root ball is saturated
+// If both acorns germinate, cut off the weaker one's stem.
+// If seedlings outgrow their pots, transfer them to 2 qt. nursery pots.
+// If saplings outgrow their pots, plant the tree in the ground:
+	// Loosen root ball and trim any kinked roots
+	// Dig a 4 ft. wide hole as deep as the root ball
+  // Replace soil on top of the root ball
+// Until trees matures, deeply water tree every week
