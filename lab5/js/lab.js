a75dbcfb0106332b40fc2d335cefd00b46393d7b
Julian Johnson <jusjohns@ucsc.edu>
2022-04-20 20:26:29 -0700

    Added lab5

---
+++ lab5/js/lab.js
@@ -0,0 +1,19 @@
+/*
+ * Authors: Julian Johnson <jusjohns@ucsc.edu>, Bryan Long <blong3@ucsc.edu>
+ * Created: 4 April
+ * License: Public Domain
+ */
+
+make = "Audi";
+model = "A3";
+color = "Silver";
+year = "1996";
+currentYear = 2022;
+age = currentYear - year;
+
+
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
